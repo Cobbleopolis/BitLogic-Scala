48e7405ddc064a111b792a54b31695a058d46ef1
Cobbleopolis <cobbleopolis@gmail.com>
2017-02-01 22:36:22 -0500

    Added background color animation and moved assets

--- core/src/scala/com/cobble/bitlogic/BitLogic.scala
+++ core/src/scala/com/cobble/bitlogic/BitLogic.scala
@@ -1,23 +1,20 @@
 package com.cobble.bitlogic
 
 import com.badlogic.gdx.{ApplicationAdapter, Gdx}
-import com.badlogic.gdx.graphics.{GL20, Texture}
+import com.badlogic.gdx.graphics.{Color, GL20, Texture}
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.utils.Logger
 
 class BitLogic extends ApplicationAdapter {
 
-    case class Color(var r: Float, var g: Float, var b: Float) {
-        override def toString: String = s"($r, $g, $b)"
-    }
-
     lazy val batch: SpriteBatch = new SpriteBatch()
-    lazy val img: Texture = new Texture("badlogic.jpg")
+    lazy val img: Texture = new Texture(Gdx.files.internal("badlogic.jpg"))
 
+    val rv: Float = 0.01f
     var ratio: Float = 0.0f
-    var close: Color = Color(Math.random.toFloat, Math.random.toFloat, Math.random.toFloat)
-    var far: Color = Color(Math.random.toFloat, Math.random.toFloat, Math.random.toFloat)
-    var currentColor: Color = close
+    var close: Color = new Color(Math.random.toFloat, Math.random.toFloat, Math.random.toFloat, 1.0f)
+    var far: Color = new Color(Math.random.toFloat, Math.random.toFloat, Math.random.toFloat, 1.0f)
+    var currentColor: Color = new Color()
     //    val near
 
     override def create(): Unit = {
@@ -25,19 +22,22 @@ class BitLogic extends ApplicationAdapter {
     }
 
     def tick(): Unit = {
+        if (ratio >= 1.0f) {
+            ratio = 0.0f
+            close.set(far)
+            far.set(Math.random().toFloat, Math.random().toFloat, Math.random().toFloat, 1.0f)
+        }
+        ratio += rv
 
-        currentColor.r += getDelta
-        currentColor.g += getDelta
-        currentColor.b += getDelta
-        currentColor.r = Math.max(Math.min(currentColor.r, 1.0), 0.0).toFloat
-        currentColor.g = Math.max(Math.min(currentColor.g, 1.0), 0.0).toFloat
-        currentColor.b = Math.max(Math.min(currentColor.b, 1.0), 0.0).toFloat
-        Gdx.app.log("BitLogic", currentColor.toString())
+        currentColor = close.cpy().lerp(far, ratio)
+//        Gdx.app.log("BitLogic|Ratio", s"$ratio | $rv")
+//        Gdx.app.log("BitLogic|Color", s"$currentColor")
+//        Gdx.app.log("BitLogic|Points", s"$close | $far")
     }
 
     override def render(): Unit = {
         tick()
-        Gdx.gl.glClearColor(currentColor.r, currentColor.g, currentColor.b, 1)
+        Gdx.gl.glClearColor(currentColor.r, currentColor.g, currentColor.b, currentColor.a)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
         batch.begin()
         batch.draw(img, 0, 0)
@@ -48,11 +48,4 @@ class BitLogic extends ApplicationAdapter {
         batch.dispose()
         img.dispose()
     }
-
-    def getDelta: Float = {
-        val offset: Float = 0.5f
-        val scale: Float = 0.0125f
-        (Math.random.toFloat * scale) - (offset * scale)
-    }
-
 }
